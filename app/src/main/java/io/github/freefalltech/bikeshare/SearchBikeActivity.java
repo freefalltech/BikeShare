ac3ca5fcb7db643c03feb474e701645b19b55c07
Abilash Senthilkumar <thenextbiggeek@users.noreply.github.com>
2017-05-26 21:37:11 +0530

    Permissions For fine location
    Permissions for fine location Updated

--- app/src/main/java/io/github/freefalltech/bikeshare/SearchBikeActivity.java
+++ app/src/main/java/io/github/freefalltech/bikeshare/SearchBikeActivity.java
@@ -2,9 +2,13 @@ package io.github.freefalltech.bikeshare;
 //26 May 2017, Coded and maintained by abilash senthilkumar. github : @nextbiggeek
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.TextViewCompat;
 import android.widget.TextView;
 
@@ -15,9 +19,9 @@ import com.google.android.gms.maps.GoogleMap;
 public class SearchBikeActivity extends FragmentActivity {
 
     private GoogleMap mMap;
-    //int permissionLocationCheckFine, permissionLocationCheckCoarse;
-   // public static final int PERMISSION_LOCATION_COARSE = 0; 
-   // public static final int PERMISSION_LOCATION_FINE = 0; 
+    int permissionLocationCheckFine, permissionLocationCheckCoarse;
+    //public static final int PERMISSION_LOCATION_COARSE = 0; 
+    //public int PERMISSION_LOCATION_FINE = 0; 
 
 
 
@@ -26,20 +30,58 @@ public class SearchBikeActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_bike);
         formatTextviews();
+        checkForPermissions();
 
+    }
+
+    private void checkForPermissions() {
+        //check if permissions are granted, and then continue
+        permissionLocationCheckFine = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if(permissionLocationCheckFine != PackageManager.PERMISSION_GRANTED){
+            //permission not granted. proceed to ask
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    12);
+
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 12: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay!
+
+
+
+                } else {
+
+                    // permission denied, boo!
+                    //for now, the app keeps asking until the user agrees. NOTE: ITS A BAD UX PRACTICE
+                    checkForPermissions();
+                }
+                return;
+            }
+
+        }
     }
 
     private void formatTextviews() {
         //initialize typeface
         Typeface helvetica = Typeface.createFromAsset(getAssets(), "fonts/helvetica.ttf");
+        Typeface arialblack = Typeface.createFromAsset(getAssets(), "fonts/arialblack.ttf");
+        Typeface adam = Typeface.createFromAsset(getAssets(), "fonts/adam.otf");
+
+
 
 
         //initialize txtviews
-        TextView openerMainText = (TextView) findViewById(R.id.opener_main_text);
-
-
-        //apply the typeface
-        openerMainText.setTypeface(helvetica);
 
 
     }
