abe59972a25758c94c9315a772fef85fa668d8a5
Abilash Senthilkumar <thenextbiggeek@users.noreply.github.com>
2017-05-26 20:07:49 +0530

    Refreshing the SearchBikeActivity
    dependency added for a gif api
    code made clean to start with
    the design of the ui is shaping up

--- app/src/main/java/io/github/freefalltech/bikeshare/SearchBikeActivity.java
+++ app/src/main/java/io/github/freefalltech/bikeshare/SearchBikeActivity.java
@@ -1,31 +1,23 @@
 package io.github.freefalltech.bikeshare;
+//26 May 2017, Coded and maintained by abilash senthilkumar. github : @nextbiggeek
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
+
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.view.View;
-import android.widget.Toast;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.TextViewCompat;
+import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class SearchBikeActivity extends FragmentActivity implements OnMapReadyCallback {
+
+
+public class SearchBikeActivity extends FragmentActivity {
 
     private GoogleMap mMap;
-    int permissionLocationCheckFine, permissionLocationCheckCoarse;
-    public static final int PERMISSION_LOCATION_COARSE = 0; 
-    public static final int PERMISSION_LOCATION_FINE = 0; 
+    //int permissionLocationCheckFine, permissionLocationCheckCoarse;
+   // public static final int PERMISSION_LOCATION_COARSE = 0; 
+   // public static final int PERMISSION_LOCATION_FINE = 0; 
 
 
 
@@ -33,115 +25,24 @@ public class SearchBikeActivity extends FragmentActivity implements OnMapReadyCa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_bike);
-        getPermissionsFromUser();
+        formatTextviews();
 
     }
 
-    //obtain permission from user
-    public void getPermissionsFromUser() { 
-        permissionLocationCheckCoarse = ContextCompat.checkSelfPermission(SearchBikeActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION); 
-        permissionLocationCheckFine = ContextCompat.checkSelfPermission(SearchBikeActivity.this, Manifest.permission.ACCESS_FINE_LOCATION); 
-        if (permissionLocationCheckFine != PackageManager.PERMISSION_GRANTED){ 
-            //not granted fine location services 
-            ActivityCompat.requestPermissions(SearchBikeActivity.this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSION_LOCATION_FINE);
-        } else if(permissionLocationCheckCoarse != PackageManager.PERMISSION_GRANTED) { 
+    private void formatTextviews() {
+        //initialize typeface
+        Typeface helvetica = Typeface.createFromAsset(getAssets(), "fonts/helvetica.ttf");
 
-            //not granted coarse location services 
-            ActivityCompat.requestPermissions(SearchBikeActivity.this,
-                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                    PERMISSION_LOCATION_COARSE);
 
-        }else{
-            // permission already granted. continue running as usual  
-        }
+        //initialize txtviews
+        TextView openerMainText = (TextView) findViewById(R.id.opener_main_text);
+
+
+        //apply the typeface
+        openerMainText.setTypeface(helvetica);
+
 
     }
 
-    LocationManager locationManager;
-    LocationListener locationListener;
-
-    private void continueListeningLocation() {
-
-        //string value to interchange between gps or network provider
-        String  locationProvider = LocationManager.GPS_PROVIDER;
-
-
-        //aquire reference to locationmanager
-         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
-                // Define a listener that responds to location updates
-                 locationListener = new LocationListener() {
-                    public void onLocationChanged(Location location) {
-                        // Called when a new location is found by the network location provider.
-                        makeUseOfNewLocation(location);
-                    }
-
-                    public void onStatusChanged(String provider, int status, Bundle extras) {
-                    }
-
-                    public void onProviderEnabled(String provider) {
-                    }
-
-                    public void onProviderDisabled(String provider) {
-                    }
-                };
-
-        // Register the listener with the Location Manager to receive location updates
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED) {
-            locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
-
-            //a
-
-            return;
-        }
-
-    }
-
-    public void stopListeningLocation(){
-        locationManager.removeUpdates(locationListener);
-    }
-
-
-    public void locationStartListening(View v){
-        //user clicks listen to location button
-        Toast.makeText(this, "app starts listening location", Toast.LENGTH_SHORT).show();
-        continueListeningLocation();
-    }
-
-    public void locationStopListening(View v){
-        //user clicks listen to location button
-        Toast.makeText(this, "app stops listening location", Toast.LENGTH_SHORT).show();
-        stopListeningLocation();
-    }
-
-
-    /**
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_LOCATION_FINE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! \
-                    continueListeningLocation();
-
-                } else {
-
-                    // permission denied, boo!
-                    //TODO HANDLE THE RESPONSE
-                }
-                return;
-            }
-
-        }
-    }**/
 
 }
