74da393fe4abf62a04e90669b95bd01cfe413358
Abilash Senthilkumar <thenextbiggeek@users.noreply.github.com>
2017-05-27 02:43:47 +0530

    created an instance of GoogleApiClient
    Created instance of GoogleApiClient
    implemented callback listeners
    changed fine location to coarse location temporarily
    added play services dependency in build.gradle of the app project

--- app/src/main/java/io/github/freefalltech/bikeshare/SearchBikeActivity.java
+++ app/src/main/java/io/github/freefalltech/bikeshare/SearchBikeActivity.java
@@ -5,24 +5,34 @@ package io.github.freefalltech.bikeshare;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.TextViewCompat;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient.Builder;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import com.google.android.gms.maps.LocationSource;
 
 
-
-public class SearchBikeActivity extends FragmentActivity {
+public class SearchBikeActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
+    private Location mLastLocation;
     int permissionLocationCheckFine, permissionLocationCheckCoarse;
     //public static final int PERMISSION_LOCATION_COARSE = 0; 
     //public int PERMISSION_LOCATION_FINE = 0; 
-
+    GoogleApiClient mGoogleApiClient;
 
 
     @Override
@@ -32,15 +42,25 @@ public class SearchBikeActivity extends FragmentActivity {
         formatTextviews();
         checkForPermissions();
 
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+
     }
 
     private void checkForPermissions() {
         //check if permissions are granted, and then continue
         permissionLocationCheckFine = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
-        if(permissionLocationCheckFine != PackageManager.PERMISSION_GRANTED){
+        if (permissionLocationCheckFine != PackageManager.PERMISSION_GRANTED) {
             //permission not granted. proceed to ask
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
                     12);
 
 
@@ -57,7 +77,6 @@ public class SearchBikeActivity extends FragmentActivity {
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
                     // permission was granted, yay!
-
 
 
                 } else {
@@ -79,12 +98,49 @@ public class SearchBikeActivity extends FragmentActivity {
         Typeface adam = Typeface.createFromAsset(getAssets(), "fonts/adam.otf");
 
 
-
-
         //initialize txtviews
 
 
     }
 
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
 
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(mLastLocation != null){
+            String locationCoordinate = String.valueOf(mLastLocation.getLatitude()+mLastLocation.getLongitude());
+            Toast.makeText(this, locationCoordinate, Toast.LENGTH_LONG);
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
