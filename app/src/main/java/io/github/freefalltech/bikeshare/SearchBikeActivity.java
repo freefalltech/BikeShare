afe71de7e3d86341cdffe64ff2f8d6632ccd8ec2
Abilash Senthilkumar <thenextbiggeek@users.noreply.github.com>
2017-05-27 23:44:32 +0530

    User Location and Address is now findable
    Coarse location reverted to fine
    User's location is returned using a location object
    intent service init for finding address
    constants class to pass data to intent filter

--- app/src/main/java/io/github/freefalltech/bikeshare/SearchBikeActivity.java
+++ app/src/main/java/io/github/freefalltech/bikeshare/SearchBikeActivity.java
@@ -3,35 +3,48 @@ package io.github.freefalltech.bikeshare;
 
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Typeface;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.TextViewCompat;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.api.GoogleApiClient.Builder;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.maps.GoogleMap;
 
-import com.google.android.gms.maps.LocationSource;
+import java.text.DateFormat;
+import java.util.Date;
 
 
-public class SearchBikeActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class SearchBikeActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener  {
 
     private GoogleMap mMap;
-    private Location mLastLocation;
-    int permissionLocationCheckFine, permissionLocationCheckCoarse;
-    //public static final int PERMISSION_LOCATION_COARSE = 0; 
-    //public int PERMISSION_LOCATION_FINE = 0; 
+    private Location mLastLocation, mCurrentLocation;
+    private LocationRequest mLocationRequest;
+    boolean mRequestLocationUpdates;
+    TextView coordinateTextView, addressTextView, lastUpdatedTime;
+    String mLastUpdateTime, mAddressOutput;
+    private ResultReceiver mResultReceiver;
+    int permissionLocationCheckFine;
     GoogleApiClient mGoogleApiClient;
 
 
@@ -41,6 +54,17 @@ public class SearchBikeActivity extends FragmentActivity implements GoogleApiCli
         setContentView(R.layout.activity_search_bike);
         formatTextviews();
         checkForPermissions();
+        mResultReceiver = new AddressResultReceiver(new Handler());
+
+
+
+    }
+
+    public void letsStartListeningLocation(View v){
+
+        mRequestLocationUpdates = true;
+
+
 
         // Create an instance of GoogleAPIClient.
         if (mGoogleApiClient == null) {
@@ -49,18 +73,117 @@ public class SearchBikeActivity extends FragmentActivity implements GoogleApiCli
                     .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
                     .build();
+
+            mGoogleApiClient.connect();
         }
+        //mLocationRequest is initialized
+
+        createLocationRequest();
+
+        //then the locationSettings builder is also initialized
+        LocationSettingsRequest.Builder mLocationSettingsRequest = new
+                LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient,
+                        mLocationSettingsRequest.build());
 
 
     }
 
+    public void letsStopListeningLocation(View v){
+        mGoogleApiClient.disconnect();
+        Toast.makeText(this, "LocationListener turned off", Toast.LENGTH_SHORT);
+    }
+
+
+
+
+
+    @Override
+    protected void onStop() {
+        //mGoogleApiClient.disconnect();
+        super.onStop();
+
+    }
+
+    protected void createLocationRequest(){
+        //sets settings for the locationrequest
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            /* allotted interval is 10 sec, and the fastest interval it can recieve
+            due to other apps is 5 seconds to revent exceptions
+            */
+            mLocationRequest.setInterval(10000);
+            mLocationRequest.setFastestInterval(5000);
+    }
+
+    public void checkAddress(View v){
+        startAddressIntentService();
+
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        if(mRequestLocationUpdates){
+           LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        updateUi();
+
+
+
+    }
+
+    private void updateUi() {
+        lastUpdatedTime.setText(mLastUpdateTime);
+        coordinateTextView.setText("Latitude is " + String.valueOf(mCurrentLocation.getLatitude())
+                + "Longitude is" + String.valueOf(mCurrentLocation.getLongitude()));
+    }
+
+
+
+
+    protected void startAddressIntentService(){
+        Intent intent = new Intent(this, FetchAddressIntentService.class);
+        intent.putExtra(Constants.RECEIVER, mResultReceiver);
+        intent.putExtra(Constants.LOCATION_DATA_EXTRA, mCurrentLocation);
+        startService(intent);
+    }
+
+
+    //permissions
     private void checkForPermissions() {
         //check if permissions are granted, and then continue
-        permissionLocationCheckFine = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        permissionLocationCheckFine = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
         if (permissionLocationCheckFine != PackageManager.PERMISSION_GRANTED) {
             //permission not granted. proceed to ask
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     12);
 
 
@@ -99,48 +222,36 @@ public class SearchBikeActivity extends FragmentActivity implements GoogleApiCli
 
 
         //initialize txtviews
+        coordinateTextView = (TextView) findViewById(R.id.coordinateTextView);
+        addressTextView = (TextView) findViewById(R.id.addressTextView);
+        lastUpdatedTime = (TextView) findViewById(R.id.lastUpdatedTime);
 
 
     }
 
-    @Override
-    protected void onStart() {
-        mGoogleApiClient.connect();
-        super.onStart();
 
-    }
-
-    @Override
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
-
-    }
-
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if(mLastLocation != null){
-            String locationCoordinate = String.valueOf(mLastLocation.getLatitude()+mLastLocation.getLongitude());
-            Toast.makeText(this, locationCoordinate, Toast.LENGTH_LONG);
+    class AddressResultReceiver extends ResultReceiver {
+        public AddressResultReceiver(Handler handler) {
+            super(handler);
         }
 
-    }
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
 
-    @Override
-    public void onConnectionSuspended(int i) {
+            // Display the address string
+            // or an error message sent from the intent service.
+            mAddressOutput = resultData.getString(Constants.RESULT_DATA_KEY);
+            Log.v("TAG", "SUCCESS");
+            addressTextView.setText(mAddressOutput);
 
-    }
+            // Show a toast message if an address was found.
+            if (resultCode == Constants.SUCCESS_RESULT) {
+                //Toast.makeText(this, "address found", Toast.LENGTH_SHORT);
+            }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        }
     }
 }
+
+
+
